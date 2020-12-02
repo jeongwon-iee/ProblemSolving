8cf1d7223b6cc6b6dab4a289bb941dd787f6d012
이정원 <45806836+jeongwon-iee@users.noreply.github.com>
2020-12-02 15:03:18 +0900

    Update 228. Summary Ranges.java

--- LeetCode/228. Summary Ranges.java
+++ LeetCode/228. Summary Ranges.java
@@ -2,18 +2,25 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> summaryOfTheRange = new ArrayList<String>();
         if(nums.length == 0) return summaryOfTheRange;
-        String subRange = new Integer(nums[0]).toString();
+        
+        String subRange="";
+        int start=nums[0];
         for(int i=1;i<nums.length;i++) {
             if(nums[i-1]+1 != nums[i]) { //subRange끊고 마무리
-                if(Integer.parseInt(subRange) != nums[i-1]) subRange+=("->"+nums[i-1]);
+                subRange = getRange(start, nums[i-1]);
                 summaryOfTheRange.add(subRange);
-                subRange = new Integer(nums[i]).toString();
-            }
-            else if(i==nums.length-1) {
-                subRange+=("->"+nums[i]);
+                start = nums[i];
             }
         }
+        subRange = getRange(start, nums[nums.length-1]);
         summaryOfTheRange.add(subRange);
         return summaryOfTheRange;
+    }
+    
+    public String getRange(int start, int end) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(start);
+        if(start != end) sb.append("->").append(end);
+        return sb.toString();
     }
 }
