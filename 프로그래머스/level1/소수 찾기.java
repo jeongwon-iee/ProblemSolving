32ebd537ef23df25e8b6d90b29ba8ce1aec26cac
이정원 <45806836+jeongwon-iee@users.noreply.github.com>
2021-01-22 15:38:04 +0900

    Create 소수 찾기.java

---
+++ 프로그래머스/level1/소수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        boolean isPrime;
+        
+        for(int i=3; i<=n; i++) {
+            isPrime = true;
+            for(int j=2; j*j<=i; j++) {
+                if(i%j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) answer++;
+        }
+        return answer;
+    }
+}
