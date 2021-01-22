a6c672d5791bd5431fcdc64688907e4d96bcc33b
이정원 <45806836+jeongwon-iee@users.noreply.github.com>
2021-01-22 09:50:09 +0900

    Create 시저 암호.java

---
+++ 프로그래머스/level1/시저 암호.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder answer = new StringBuilder();
+        char[] upperAlphabet = new char[26];
+        char[] lowerAlphabet = new char[26];
+        
+        for(int i=0; i<26; i++) {
+            upperAlphabet[i] = (char)('A'+i);
+            lowerAlphabet[i] = (char)('a'+i);
+        }
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                answer.append(" ");
+                continue;
+            }
+            char c = s.charAt(i);
+            if(Character.isUpperCase(c)) {
+                answer.append(upperAlphabet[(c-65+n)%26]);
+            } else if(Character.isLowerCase(c)) {
+                answer.append(lowerAlphabet[(c-97+n)%26]);
+            }
+        }
+        return answer.toString();
+    }
+}
