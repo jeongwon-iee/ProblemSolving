6b648a4c102c4d2d5d62309a8d29ab59afe40624
이정원 <45806836+jeongwon-iee@users.noreply.github.com>
2021-01-24 20:56:37 +0900

    Update 정수 제곱근 판별.java

--- 프로그래머스/level1/정수 제곱근 판별.java
+++ 프로그래머스/level1/정수 제곱근 판별.java
@@ -1,7 +1,7 @@
 class Solution {
     public long solution(long n) {
-        double target = Math.sqrt(n);
-        if(target - (int)target == 0) return (long)(target+1)*(long)(target+1);
+        if(Math.pow((int)Math.sqrt(n), 2) == n) 
+            return (long)Math.pow((int)Math.sqrt(n)+1, 2);
         return -1;
     }
 }
