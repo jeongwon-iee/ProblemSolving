6ef411ade19d3785749a7f5c4a1ef2840dacf922
이정원 <garden1117244@gmail.com>
2021-03-02 13:47:55 +0900

    Create 4796.java

---
+++ Backjoon Online Judge/4796.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = -1, P = -1, V = -1;
+        int testcase = 1;
+        while (testcase > 0) {
+            L = sc.nextInt();
+            P = sc.nextInt();
+            V = sc.nextInt();
+            if (L == 0 && P == 0 && V == 0) {
+                return;
+            }
+            System.out.println("Case " + testcase++ + ": " + findMaxCampingAvailable(L, P, V));
+        }
+    }
+
+    static int findMaxCampingAvailable(int campAvailable, int totalDays, int vacation) {
+        int totalAvailableDays = vacation / totalDays * campAvailable;
+        totalAvailableDays += Math.min((vacation % totalDays), campAvailable);
+        return Math.min(totalAvailableDays, vacation);
+    }
+}
