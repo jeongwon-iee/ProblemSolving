8f584ea21ea641bbf95f54e9f9b9bdc2290ad70d
이정원 <garden1117244@gmail.com>
2021-04-12 16:53:34 +0900

    Update 0053. Maximum Subarray.java

--- LeetCode/0053. Maximum Subarray.java
+++ LeetCode/0053. Maximum Subarray.java
@@ -12,3 +12,18 @@ class Solution {
         return maxSum;
     }
 }
+/*
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 0) return 0;
+        
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
+*/
