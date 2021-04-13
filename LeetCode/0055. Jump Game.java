8e20b9576bb75bea93dc6e862a0c459bf24edddd
이정원 <garden1117244@gmail.com>
2021-04-13 17:58:47 +0900

    Create 0055. Jump Game.java

---
+++ LeetCode/0055. Jump Game.java
@@ -0,0 +1,31 @@
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+class Solution {
+    Index[] memo;
+    public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for(int i=0; i<memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[nums.length-1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
+    }
+    
+    boolean canJumpFromPosition(int position, int[] nums) {
+        if(memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD? true: false;
+        }
+        
+        int furthestJump = Math.min(position+nums[position], nums.length-1);
+        for(int nextPosition = position+1; nextPosition<=furthestJump; nextPosition++) {
+            if(canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+        
+        memo[position] = Index.BAD;
+        return false;
+    }
+}
